692d72cb312cf68ad85a417a61f29fd9cd534d18
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-12-11 10:11:59 +0800

    add java sources and native libs

---
+++ src/main/java/com/github/teamhartex/jetbrainsplugins/discordrichpresence/Plugin.java
@@ -0,0 +1,7 @@
+package com.github.teamhartex.jetbrainsplugins.discordrichpresence;
+
+import com.intellij.openapi.diagnostic.Logger;
+
+public final class Plugin {
+    public static final Logger LOGGER = Logger.getInstance("DiscordRichPresence");
+}
